e5da909cde87b8fe2eed46d5d12fd0b2bfe4bb6c
HenryTung <henryTung@kosbrother.com>
2016-04-11 16:39:52 +0800

    Add getGcmToken at application

--- app/src/main/java/com/kosbrother/mongmongwoo/AnalyticsApplication.java
+++ app/src/main/java/com/kosbrother/mongmongwoo/AnalyticsApplication.java
@@ -1,9 +1,14 @@
 package com.kosbrother.mongmongwoo;
 
 import android.app.Application;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
+import com.kosbrother.mongmongwoo.gcm.GcmPreferences;
+import com.kosbrother.mongmongwoo.gcm.RegistrationIntentService;
 
 /**
  * Created by kolichung on 3/31/16.
@@ -11,8 +16,25 @@ import com.google.android.gms.analytics.Tracker;
 public class AnalyticsApplication extends Application {
     private Tracker mTracker;
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        getGcmToken();
+    }
+
+    private void getGcmToken() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean sentToken = sharedPreferences
+                .getBoolean(GcmPreferences.SENT_TOKEN_TO_SERVER, false);
+        if (!sentToken) {
+            // Start IntentService to register this application with GCM.
+            startService(new Intent(this, RegistrationIntentService.class));
+        }
+    }
+
     /**
      * Gets the default {@link Tracker} for this {@link Application}.
+     *
      * @return tracker
      */
     synchronized public Tracker getDefaultTracker() {
