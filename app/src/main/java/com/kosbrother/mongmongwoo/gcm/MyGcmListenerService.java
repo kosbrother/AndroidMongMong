f50edc39594fcb8f3fe8e907bf70c8635e7fcf74
HenryTung <henrytung@kosbrother.com>
2016-04-12 15:37:30 +0800

    Update sendNotification with real input data

--- app/src/main/java/com/kosbrother/mongmongwoo/gcm/MyGcmListenerService.java
+++ app/src/main/java/com/kosbrother/mongmongwoo/gcm/MyGcmListenerService.java
@@ -4,18 +4,28 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.BigPictureStyle;
 
-import com.kosbrother.mongmongwoo.MainActivity;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.kosbrother.mongmongwoo.ProductActivity;
 import com.kosbrother.mongmongwoo.R;
+import com.kosbrother.mongmongwoo.model.Product;
 
 public class MyGcmListenerService extends com.google.android.gms.gcm.GcmListenerService {
 
     private static final String TAG = "MyGcmListenerService";
+    private static final String HOST = "http://api.kosbrother.com";
 
     /**
      * Called when message is received.
@@ -26,39 +36,42 @@ public class MyGcmListenerService extends com.google.android.gms.gcm.GcmListener
      */
     // [START receive_message]
     @Override
-    public void onMessageReceived(String from, Bundle data) {
-        String message = data.getString("message");
+    public void onMessageReceived(String from, final Bundle data) {
+        Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                SimpleTarget target = new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(Bitmap bitmap, GlideAnimation glideAnimation) {
+                        // do something with the bitmap
+                        // for demonstration purposes, let's just set it to an ImageView
+                        sendNotification(data, bitmap);
+                    }
+                };
 
-        if (from.startsWith("/topics/")) {
-            // message received from some topic.
-        } else {
-            // normal downstream message.
-        }
-
-        // [START_EXCLUDE]
-        /**
-         * Production applications would usually process the message here.
-         * Eg: - Syncing with server.
-         *     - Store message in local database.
-         *     - Update UI.
-         */
-
-        /**
-         * In some cases it may be useful to show a notification indicating to the user
-         * that a message was received.
-         */
-        sendNotification(message);
-        // [END_EXCLUDE]
+                //noinspection unchecked
+                Glide.with(getApplicationContext())
+                        .load(HOST + data.getString("content_pic"))
+                        .asBitmap()
+                        .into(target);
+            }
+        };
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        mainHandler.post(myRunnable);
     }
     // [END receive_message]
 
     /**
      * Create and show a simple notification containing the received GCM message.
      *
-     * @param message GCM message received.
+     * @param data   GCM message received.
+     * @param bitmap bitmap for notification big picture style
      */
-    private void sendNotification(String message) {
-        Intent intent = new Intent(this, MainActivity.class);
+    private void sendNotification(Bundle data, Bitmap bitmap) {
+        Intent intent = new Intent(this, ProductActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("Selected_Product", getProduct(data));
+        intent.putExtras(bundle);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
@@ -67,15 +80,31 @@ public class MyGcmListenerService extends com.google.android.gms.gcm.GcmListener
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.app_icon9))
                 .setSmallIcon(R.mipmap.icon_mhouse)
-                .setContentTitle("GCM Message")
-                .setContentText(message)
+                .setContentTitle(data.getString("content_title"))
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+                .setContentIntent(pendingIntent)
+                .setStyle(getBigPictureStyle(data.getString("content_text"), bitmap));
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    @NonNull
+    private BigPictureStyle getBigPictureStyle(String contentText, Bitmap bitmap) {
+        BigPictureStyle style = new BigPictureStyle().bigPicture(bitmap);
+        style.setSummaryText(contentText);
+        return style;
+    }
+
+    @NonNull
+    private Product getProduct(Bundle data) {
+        return new Product(
+                Integer.parseInt(data.getString("item_id")),
+                data.getString("item_name"),
+                Integer.parseInt(data.getString("item_price")),
+                "");
     }
 }
