615b693253582ebcada5e378ce3e1393c5ec4436
HenryTung <henrytung@kosbrother.com>
2016-04-13 18:12:05 +0800

    Replace updateProductByIdAsyncTask to RxJava

--- app/src/main/java/com/kosbrother/mongmongwoo/api/WebService.java
+++ app/src/main/java/com/kosbrother/mongmongwoo/api/WebService.java
@@ -1,9 +1,12 @@
 package com.kosbrother.mongmongwoo.api;
 
+import com.kosbrother.mongmongwoo.model.Product;
+
 import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class WebService {
@@ -15,7 +18,31 @@ public class WebService {
                 subscriber.onNext(UserApi.postUser(userJsonString));
                 subscriber.onCompleted();
             }
-        }).subscribeOn(Schedulers.newThread())
+        })
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(onNextAction);
+    }
+
+    public static void updateProductById(final Product product, Action1<Product> onNextAction) {
+        Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext(ProductApi.getMessageFromServer(
+                        "GET", null, null, UrlCenter.updateProductById(product.getId())));
+                subscriber.onCompleted();
+            }
+        })
+                .map(new Func1<String, Product>() {
+                    @Override
+                    public Product call(String message) {
+                        if (message == null) {
+                            return null;
+                        }
+                        return ProductApi.parseItem(message, product);
+                    }
+                })
+                .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(onNextAction);
     }
