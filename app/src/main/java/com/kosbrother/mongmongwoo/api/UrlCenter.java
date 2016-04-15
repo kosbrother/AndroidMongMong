5c81ba71a75ce93c63334a0d2ff5812b44100240
HenryTung <henrytung@kosbrother.com>
2016-04-15 11:35:25 +0800

    Add test host url

--- app/src/main/java/com/kosbrother/mongmongwoo/api/UrlCenter.java
+++ app/src/main/java/com/kosbrother/mongmongwoo/api/UrlCenter.java
@@ -1,8 +1,12 @@
 package com.kosbrother.mongmongwoo.api;
 
+import com.kosbrother.mongmongwoo.BuildConfig;
+
 public class UrlCenter {
 
-    private static final String HOST = "http://api.mmwooo.com";
+    public static final String HOST_TEST = "http://106.185.25.83";
+    public static final String HOST_PRD = "http://api.mmwooo.com";
+    public static final String HOST = BuildConfig.DEBUG ? HOST_TEST : HOST_PRD;
     public static final String API_V1 = "/api/v1";
     public static final String API_V2 = "/api/v2";
 
