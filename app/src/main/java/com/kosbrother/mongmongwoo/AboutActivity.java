256b9d37945edc2136cb9e621b0c7b7d8c396ef8
HenryTung <henrytung@kosbrother.com>
2016-04-27 09:59:02 +0800

    Add update version feature to about activity

--- app/src/main/java/com/kosbrother/mongmongwoo/AboutActivity.java
+++ app/src/main/java/com/kosbrother/mongmongwoo/AboutActivity.java
@@ -1,28 +1,63 @@
 package com.kosbrother.mongmongwoo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.kosbrother.mongmongwoo.api.UrlCenter;
 
 /**
  * Created by kolichung on 3/10/16.
  */
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        setToolbar();
+        setVersionName();
+        setViewByVersionUpToDate();
+    }
 
+    private void setToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.icon_back_white);
         toolbar.setTitleTextColor(0xFFFFFFFF);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("關於我們");
+    }
 
+    private void setVersionName() {
+        TextView versionNameTv = (TextView) findViewById(R.id.version_name_tv);
+        versionNameTv.setText(Settings.getVersionName(this));
+    }
+
+    private void setViewByVersionUpToDate() {
+        View updateBtn = findViewById(R.id.update_btn);
+        View upToDateTv = findViewById(R.id.up_to_date_tv);
+        if (Settings.isUpToDate(this)) {
+            upToDateTv.setVisibility(View.VISIBLE);
+            updateBtn.setVisibility(View.GONE);
+        } else {
+            upToDateTv.setVisibility(View.GONE);
+            updateBtn.setVisibility(View.VISIBLE);
+            updateBtn.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Uri uri = Uri.parse(UrlCenter.GOOGLE_PLAY);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 
     @Override
@@ -37,6 +72,4 @@ public class AboutActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(menuItem);
     }
-
-
 }
