c7efd9f4a2e9b2b7cc9da19cf958f38381d5f9b1
HenryTung <henrytung@kosbrother.com>
2016-04-29 16:15:15 +0800

    Update getCounties api

--- app/src/main/java/com/kosbrother/mongmongwoo/api/StoreApi.java
+++ app/src/main/java/com/kosbrother/mongmongwoo/api/StoreApi.java
@@ -29,7 +29,7 @@ public class StoreApi {
 
     public static ArrayList<County> getCounties() {
         ArrayList<County> counties = new ArrayList<>();
-        String message = "[{\"id\":1,\"name\":\"台北市\"},{\"id\":2,\"name\":\"基隆市\"},{\"id\":3,\"name\":\"宜蘭縣\"},{\"id\":4,\"name\":\"新北市\"},{\"id\":5,\"name\":\"新竹市\"},{\"id\":6,\"name\":\"新竹縣\"},{\"id\":7,\"name\":\"桃園市\"},{\"id\":8,\"name\":\"苗栗縣\"},{\"id\":9,\"name\":\"台中市\"},{\"id\":10,\"name\":\"南投縣\"},{\"id\":11,\"name\":\"彰化縣\"},{\"id\":12,\"name\":\"嘉義市\"},{\"id\":13,\"name\":\"嘉義縣\"},{\"id\":14,\"name\":\"雲林縣\"},{\"id\":15,\"name\":\"台南市\"},{\"id\":16,\"name\":\"澎湖縣\"},{\"id\":17,\"name\":\"高雄市\"},{\"id\":18,\"name\":\"台東縣\"},{\"id\":19,\"name\":\"屏東縣\"},{\"id\":20,\"name\":\"花蓮縣\"}]";
+        String message = "[{id: 21,name: \"台北市\"},{id: 22,name: \"基隆市\"},{id: 23,name: \"新北市\"},{id: 24,name: \"桃園市\"},{id: 25,name: \"新竹市\"},{id: 26,name: \"新竹縣\"},{id: 27,name: \"苗栗縣\"},{id: 28,name: \"台中市\"},{id: 29,name: \"彰化縣\"},{id: 30,name: \"南投縣\"},{id: 31,name: \"雲林縣\"},{id: 32,name: \"嘉義市\"},{id: 33,name: \"嘉義縣\"},{id: 34,name: \"台南市\"},{id: 35,name: \"高雄市\"},{id: 36,name: \"屏東縣\"},{id: 37,name: \"宜蘭縣\"},{id: 38,name: \"花蓮縣\"},{id: 39,name: \"台東縣\"},{id: 40,name: \"澎湖縣\"},{id: 41,name: \"連江縣\"},{id: 42,name: \"金門縣\"}]";
         parseCounties(counties, message);
         return counties;
     }
