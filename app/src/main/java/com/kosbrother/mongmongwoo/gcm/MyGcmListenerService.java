7810fd8dde2e51d3e63e68e0af932a7d05158b5b
HenryTung <henrytung@kosbrother.com>
2016-05-09 13:11:48 +0800

    Add send order delivery notification

--- app/src/main/java/com/kosbrother/mongmongwoo/gcm/MyGcmListenerService.java
+++ app/src/main/java/com/kosbrother/mongmongwoo/gcm/MyGcmListenerService.java
@@ -1,13 +1,5 @@
 package com.kosbrother.mongmongwoo.gcm;
 
-import com.google.android.gms.analytics.HitBuilders;
-import com.google.android.gms.analytics.Tracker;
-
-import com.kosbrother.mongmongwoo.AnalyticsApplication;
-import com.kosbrother.mongmongwoo.ProductActivity;
-import com.kosbrother.mongmongwoo.R;
-import com.kosbrother.mongmongwoo.model.Product;
-
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -15,16 +7,21 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat.BigPictureStyle;
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+import com.kosbrother.mongmongwoo.AnalyticsApplication;
+import com.kosbrother.mongmongwoo.PastOrderDetailActivity;
+import com.kosbrother.mongmongwoo.ProductActivity;
+import com.kosbrother.mongmongwoo.R;
+import com.kosbrother.mongmongwoo.model.Product;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MyGcmListenerService extends com.google.android.gms.gcm.GcmListenerService {
@@ -41,74 +38,113 @@ public class MyGcmListenerService extends com.google.android.gms.gcm.GcmListener
     // [START receive_message]
     @Override
     public void onMessageReceived(String from, final Bundle data) {
-        try {
-            URL url = new URL(data.getString("content_pic"));
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            Bitmap bitmap = BitmapFactory.decodeStream(input);
-
-            sendNotification(data, bitmap);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (orderMessage(data)) {
+            onReceivedOrder(data);
+        } else {
+            onReceivedProduct(data);
         }
-
-
     }
     // [END receive_message]
 
-    /**
-     * Create and show a simple notification containing the received GCM message.
-     *
-     * @param data   GCM message received.
-     * @param bitmap bitmap for notification big picture style
-     */
-    private void sendNotification(Bundle data, Bitmap bitmap) {
-        Intent intent = new Intent(this, ProductActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("Selected_Product", getProduct(data));
-        intent.putExtras(bundle);
-        intent.putExtra(ProductActivity.BOOLEAN_EXTRA_FROM_NOTIFICATION, true);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
+    private void onReceivedOrder(Bundle data) {
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.app_icon9))
+                .setSmallIcon(R.mipmap.ic_mhouse)
+                .setContentTitle(data.getString("content_title"))
+                .setContentText(data.getString("content_text"))
+                .setContentIntent(getPastOrderPendingIntent(data))
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setAutoCancel(true);
 
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        sendNotification(notificationBuilder);
+    }
+
+    private void onReceivedProduct(Bundle data) {
+        Bitmap productBitmap = getProductBitmap(data);
+        if (productBitmap != null) {
+            sendProductNotification(data, productBitmap);
+
+            Tracker mTracker = ((AnalyticsApplication) (getApplication())).getDefaultTracker();
+            mTracker.setScreenName("Notification" + data.getString("content_title"));
+            mTracker.send(new HitBuilders.ScreenViewBuilder().build());
+        }
+    }
+
+    private void sendProductNotification(Bundle data, Bitmap bitmap) {
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.app_icon9))
                 .setSmallIcon(R.mipmap.ic_mhouse)
                 .setContentTitle(data.getString("content_title"))
                 .setContentText(data.getString("content_text"))
                 .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentIntent(getProductPendingIntent(data))
+                .setPriority(NotificationCompat.PRIORITY_MAX)
                 .setStyle(getBigPictureStyle(data.getString("content_text"), bitmap));
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
-        Tracker mTracker = ((AnalyticsApplication) (getApplication())).getDefaultTracker();
-        mTracker.setScreenName("Notification" + data.getString("content_title"));
-        mTracker.send(new HitBuilders.ScreenViewBuilder().build());
+        sendNotification(notificationBuilder);
     }
 
-    @NonNull
-    private BigPictureStyle getBigPictureStyle(String contentText, Bitmap bitmap) {
-        BigPictureStyle style = new BigPictureStyle().bigPicture(bitmap);
-        style.setSummaryText(contentText);
-        return style;
+    private Bitmap getProductBitmap(Bundle data) {
+        Bitmap bitmap = null;
+        try {
+            URL url = new URL(data.getString("content_pic"));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            bitmap = BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 
-    @NonNull
     private Product getProduct(Bundle data) {
         return new Product(
                 Integer.parseInt(data.getString("item_id")),
                 data.getString("item_name"),
                 Integer.parseInt(data.getString("item_price")),
                 "");
+    }
+
+    private PendingIntent getPastOrderPendingIntent(Bundle data) {
+        Intent pastOrderIntent = new Intent(this, PastOrderDetailActivity.class);
+        pastOrderIntent.putExtra(PastOrderDetailActivity.EXTRA_INT_ORDER_ID,
+                Integer.valueOf(data.getString("order_id")));
+        return PendingIntent.getActivity(
+                this,
+                0,
+                pastOrderIntent,
+                PendingIntent.FLAG_ONE_SHOT
+        );
+    }
+
+    private PendingIntent getProductPendingIntent(Bundle data) {
+        Intent intent = new Intent(this, ProductActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("Selected_Product", getProduct(data));
+        intent.putExtras(bundle);
+        intent.putExtra(ProductActivity.BOOLEAN_EXTRA_FROM_NOTIFICATION, true);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        return PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+    }
+
+    private BigPictureStyle getBigPictureStyle(String contentText, Bitmap bitmap) {
+        BigPictureStyle style = new BigPictureStyle().bigPicture(bitmap);
+        style.setSummaryText(contentText);
+        return style;
+    }
+
+    private boolean orderMessage(Bundle data) {
+        return data.containsKey("order_id");
+    }
+
+    private void sendNotification(NotificationCompat.Builder notificationBuilder) {
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
