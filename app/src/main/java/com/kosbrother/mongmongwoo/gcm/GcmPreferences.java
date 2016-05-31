84723a928e9d02933cd44e40ad33e6800557056b
HenryTung <henrytung@kosbrother.com>
2016-05-31 13:52:05 +0800

    Upgrade gcm to fcm

--- app/src/main/java/com/kosbrother/mongmongwoo/gcm/GcmPreferences.java
+++
@@ -1,7 +0,0 @@
-package com.kosbrother.mongmongwoo.gcm;
-
-public class GcmPreferences {
-
-    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
-    public static final String TOKEN = "TOKEN";
-}
