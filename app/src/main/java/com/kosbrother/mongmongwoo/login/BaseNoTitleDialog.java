6c7f663bae659b5956fcfcd11c74b2cb759e19cb
HenryTung <henrytung@kosbrother.com>
2016-07-08 14:22:19 +0800

    Fix registered behavior and modify adjust some layout

--- app/src/main/java/com/kosbrother/mongmongwoo/login/BaseNoTitleDialog.java
+++ app/src/main/java/com/kosbrother/mongmongwoo/login/BaseNoTitleDialog.java
@@ -4,7 +4,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.view.Window;
 
-public class BaseNoTitleDialog extends Dialog{
+public class BaseNoTitleDialog extends Dialog {
     public BaseNoTitleDialog(Context context) {
         super(context);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
