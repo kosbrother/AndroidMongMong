44927523396226adc55723e9c83e5bcf8f7a8032
HenryTung <henrytung@kosbrother.com>
2016-07-27 11:43:27 +0800

    Add DateFormatUtil

---
+++ app/src/main/java/com/kosbrother/mongmongwoo/utils/DateFormatUtil.java
@@ -0,0 +1,43 @@
+package com.kosbrother.mongmongwoo.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DateFormatUtil {
+
+    public static final String API_CREATED_AT_PATTERN = "yyyy-MM-dd'T'hh:mm:ss.sss";
+
+    public static String parseToMonthAndDay(String createdAt) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(API_CREATED_AT_PATTERN, Locale.getDefault());
+        try {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(simpleDateFormat.parse(createdAt));
+            return (cal.get(Calendar.MONTH) + 1) + "月" + cal.get(Calendar.DAY_OF_MONTH) + "日";
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static String parseToDateAndTime(String createdAt) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(API_CREATED_AT_PATTERN, Locale.getDefault());
+        try {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(simpleDateFormat.parse(createdAt));
+            return cal.get(Calendar.YEAR) + "/" +
+                    formatLeadingZero(cal.get(Calendar.MONTH) + 1) + "/"
+                    + cal.get(Calendar.DATE) + " "
+                    + formatLeadingZero(cal.get(Calendar.HOUR_OF_DAY)) + ":" + formatLeadingZero(cal.get(Calendar.MINUTE));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private static String formatLeadingZero(int integer) {
+        return String.format(Locale.getDefault(), "%02d", integer);
+    }
+
+}
---
+++ app/src/test/java/com/kosbrother/mongmongwoo/utils/DateFormatUtilTest.java
@@ -0,0 +1,21 @@
+package com.kosbrother.mongmongwoo.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateFormatUtilTest {
+
+    private static final String CREATED_AT = "2016-07-26T01:01:01.000+08:00";
+
+    @Test
+    public void testGetMonthAndDay() throws Exception {
+        String result = DateFormatUtil.parseToMonthAndDay(CREATED_AT);
+        Assert.assertEquals("7月26日", result);
+    }
+
+    @Test
+    public void testGetDateAndTime() throws Exception {
+        String result = DateFormatUtil.parseToDateAndTime(CREATED_AT);
+        Assert.assertEquals("2016/07/26 01:01", result);
+    }
+}
