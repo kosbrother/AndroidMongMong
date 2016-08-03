b2a3e1ce82f1c6fc403726bf0bf94e38fc1b5de3
HenryTung <henrytung@kosbrother.com>
2016-08-03 18:04:25 +0800

    Add past orders empty layout

--- app/src/main/java/com/kosbrother/mongmongwoo/pastorders/PastOrderActivity.java
+++ app/src/main/java/com/kosbrother/mongmongwoo/pastorders/PastOrderActivity.java
@@ -2,11 +2,13 @@ package com.kosbrother.mongmongwoo.pastorders;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.kosbrother.mongmongwoo.BaseActivity;
 import com.kosbrother.mongmongwoo.R;
@@ -17,22 +19,20 @@ import com.kosbrother.mongmongwoo.entity.ResponseEntity;
 import com.kosbrother.mongmongwoo.entity.postorder.PostOrder;
 import com.kosbrother.mongmongwoo.googleanalytics.GAManager;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import rx.functions.Action1;
 
 public class PastOrderActivity extends BaseActivity {
 
-    GridView mGridView;
-    List<PostOrder> postOrders = new ArrayList<>();
-    PastOrdersAdapter pastOrdersAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_past_orders);
-        setToolbar();
+        ActionBar supportActionBar = getSupportActionBar();
+        assert supportActionBar != null;
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+
+        setContentView(R.layout.loading_no_toolbar);
         getOrders();
     }
 
@@ -48,6 +48,7 @@ public class PastOrderActivity extends BaseActivity {
         if (itemId == R.id.query_item) {
             Intent intent = new Intent(this, QueryPastOrdersActivity.class);
             startActivity(intent);
+            return true;
         }
         return super.onOptionsItemSelected(menuItem);
     }
@@ -59,32 +60,35 @@ public class PastOrderActivity extends BaseActivity {
                     @Override
                     public void call(ResponseEntity<List<PostOrder>> listResponseEntity) {
                         List<PostOrder> postOrders = listResponseEntity.getData();
-                        if (postOrders != null && postOrders.size() >= 0) {
-                            PastOrderActivity.this.postOrders.addAll(postOrders);
-                            onGetDataResult();
+                        if (postOrders != null) {
+                            onGetDataResult(postOrders);
                         } else {
-                            GAManager.sendError("getOrdersByEmailError", listResponseEntity.getError());
+                            ResponseEntity.Error error = listResponseEntity.getError();
+                            Toast.makeText(PastOrderActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                            GAManager.sendError("getOrdersByEmailError", error);
                         }
                     }
                 });
     }
 
-    private void onGetDataResult() {
-        if (pastOrdersAdapter == null) {
-            pastOrdersAdapter = new PastOrdersAdapter(PastOrderActivity.this, postOrders);
-            mGridView = (GridView) findViewById(R.id.fragment_gridview);
+    private void onGetDataResult(final List<PostOrder> postOrders) {
+        if (postOrders.size() == 0) {
+            setContentView(R.layout.activity_past_orders_empty);
+        } else {
+            setContentView(R.layout.activity_past_orders);
+            PastOrdersAdapter pastOrdersAdapter = new PastOrdersAdapter(this, postOrders);
+            GridView mGridView = (GridView) findViewById(R.id.fragment_gridview);
             mGridView.setAdapter(pastOrdersAdapter);
             mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     Intent intent = new Intent(PastOrderActivity.this, PastOrderDetailActivity.class);
                     intent.putExtra(PastOrderDetailActivity.EXTRA_INT_ORDER_ID,
-                            PastOrderActivity.this.postOrders.get(position).getId());
+                            postOrders.get(position).getId());
                     startActivity(intent);
                 }
             });
-        } else {
-            pastOrdersAdapter.notifyDataSetChanged();
+
         }
     }
 
