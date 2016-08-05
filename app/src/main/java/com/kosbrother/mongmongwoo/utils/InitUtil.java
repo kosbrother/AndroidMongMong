00b61add3434a052a91904e7577a61540e34ed8a
HenryTung <henrytung@kosbrother.com>
2016-08-05 11:47:09 +0800

    Init app at IndexActivity

---
+++ app/src/main/java/com/kosbrother/mongmongwoo/utils/InitUtil.java
@@ -0,0 +1,28 @@
+package com.kosbrother.mongmongwoo.utils;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.crashlytics.android.Crashlytics;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.kosbrother.mongmongwoo.BuildConfig;
+import com.kosbrother.mongmongwoo.Settings;
+import com.kosbrother.mongmongwoo.googleanalytics.GAManager;
+import com.kosbrother.mongmongwoo.shoppingcart.ShoppingCartManager;
+
+import io.fabric.sdk.android.Fabric;
+
+public class InitUtil {
+
+    public static void initApp(Context applicationContext, Application application) {
+        Settings.init(applicationContext);
+        GAManager.init(applicationContext);
+        FacebookSdk.sdkInitialize(applicationContext);
+        AppEventsLogger.activateApp(application);
+        ShoppingCartManager.init(applicationContext);
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(applicationContext, new Crashlytics());
+        }
+    }
+}
