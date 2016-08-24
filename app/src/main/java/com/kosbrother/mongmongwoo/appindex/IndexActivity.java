243c9e4680513498d71dd538eda373b3a4c1787c
HenryTung <henrytung@kosbrother.com>
2016-08-24 15:56:15 +0800

    Modify IndexActivity Category pattern

--- app/src/main/java/com/kosbrother/mongmongwoo/appindex/IndexActivity.java
+++ app/src/main/java/com/kosbrother/mongmongwoo/appindex/IndexActivity.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 
 import com.kosbrother.mongmongwoo.category.CategoryActivity;
 import com.kosbrother.mongmongwoo.launch.LaunchActivity;
+import com.kosbrother.mongmongwoo.model.Category;
 import com.kosbrother.mongmongwoo.product.ProductActivity;
 
 import java.io.UnsupportedEncodingException;
@@ -18,8 +19,10 @@ import java.util.regex.Pattern;
 
 public class IndexActivity extends AppCompatActivity {
 
-    private static final String PRODUCT_PATTERN = "^https:[/][/]www.mmwooo.com[/]categories[/](.+)[/]items[/](.+)$";
-    private static final String CATEGORY_PATTERN = "^https:[/][/]www.mmwooo.com[/]categories[/]([^?]+)(?:[?]sort=(\\w+))?$";
+    private static final String PRODUCT_PATTERN =
+            "^https:[/][/]www.mmwooo.com[/]categories[/](.+)[/]items[/](.+)$";
+    private static final String CATEGORY_PATTERN =
+            "^https:[/][/]www.mmwooo.com[/]categories[/]([^?]+)(?:[?](.+))?$";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,7 +62,7 @@ public class IndexActivity extends AppCompatActivity {
                 indexIntent.putExtra(ProductActivity.EXTRA_BOOLEAN_FROM_APP_INDEX, true);
             } else if (categoryMatcher.matches()) {
                 String categoryName = getDecodeString(categoryMatcher.group(1));
-                String sortName = categoryMatcher.group(2);
+                String sortName = getCategorySortName(categoryMatcher.group(2));
 
                 indexIntent = new Intent(this, CategoryActivity.class);
                 indexIntent.putExtra(CategoryActivity.EXTRA_STRING_CATEGORY_NAME, categoryName);
@@ -71,6 +74,26 @@ public class IndexActivity extends AppCompatActivity {
             startActivity(indexIntent);
             finish();
         }
+    }
+
+    private String getCategorySortName(String queryString) {
+        String sortName = Category.SortName.popular.name();
+        if (queryString == null) {
+            return sortName;
+        }
+        if (queryString.contains("&")) {
+            String[] queryArray = queryString.split("&");
+            for (String q : queryArray) {
+                if (q.contains("sort")) {
+                    sortName = q.split("=")[1];
+                }
+            }
+        } else {
+            if (queryString.contains("sort")) {
+                sortName = queryString.split("=")[1];
+            }
+        }
+        return sortName;
     }
 
     private String getDecodeString(String categoryNameString) {
